7f6c8f8b4eb6589b79304247b8cca1b04e152620
jparkinson <jamie.parkinson@gmail.com>
2020-03-22 11:39:58 +0000

    Adding build sphinx support

--- README.md
+++ README.md
@@ -44,14 +44,23 @@ print(temperature.mean())
 ```
 
 ## Documentation
-Further documentation can be found [online](https://jrgparkinson.github.io/chombopy/_build/html/). To compile the documentation locally, setup a virtual environment:
+Further documentation can be found [online](https://jrgparkinson.github.io/chombopy/_build/html/). To compile the documentation locally, get the code and setup a virtual environment:
 ```bash
+$ git clone https://github.com/jrgparkinson/chombopy.git
+$ cd chombopy
 $ python3 -m venv .venv
 $ source .venv/bin/activate
-$ pip install
+$ pip install .
 ```
 Then to build the documentation with Sphinx:
 ```bash
 $ cd docs
 $ make html 
 ```
+
+## Testing
+Unit tests can be run in a similar way to building the documentation. Having got a copy of the code and setup a virtual environment, from the root directory (i.e. `/path/to/chombopy`), simply run
+```bash
+$ py.test
+```
+Code coverage will be published to `chombopy/docs/test/coverage.html`, and test results to `chombopy/docs/test/report.html`.
--- setup.py
+++ setup.py
@@ -1,12 +1,19 @@
 from setuptools import setup, find_packages
-
 from os import path
+from sphinx.setup_command import BuildDoc
+
+cmdclass = {'build_sphinx': BuildDoc}
+
 this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
-setup(name='chombopy',
-      version='0.1.3',
+name = 'chombopy'
+version = '0.1'
+release = '0.1.3'
+
+setup(name=name,
+      version=release,
       description='Running, analysing and plotting Chombo simulations',
       long_description=long_description,
       long_description_content_type='text/markdown',
@@ -20,7 +27,9 @@ setup(name='chombopy',
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
       ],
-      setup_requires = ['wheel'],
+      setup_requires = ['wheel', 'sphinx',
+                        'recommonmark>=0.5.0',
+                        'm2r'],
       python_requires='>=3.6',
       install_requires=['matplotlib>=3.0.0',
                         'Shapely>=1.6.0',
@@ -29,9 +38,14 @@ setup(name='chombopy',
                         'scikit-image>=0.16.2',
                         'xarray>=0.11.3',
                         'h5py>=2.9.0',
-                        'numpy>=1.16.0',
-                        'sphinx',
-                        'recommonmark>=0.5.0',
-                        'm2r'],
+                        'numpy>=1.16.0'],
       tests_require=['pytest', 'coverage', 'pytest-html'],
+      cmdclass=cmdclass,
+      # these are optional and override conf.py settings
+      command_options={
+        'build_sphinx': {
+            'project': ('setup.py', name),
+            'version': ('setup.py', version),
+            'release': ('setup.py', release),
+            'source_dir': ('setup.py', 'doc')}},
       zip_safe=False)
